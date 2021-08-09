096ffa285f8c89838ac1e6f7296d0516e75522c6
Raj <growwmorenet@gmail.com>
2021-08-09 09:33:23 +0530

    new website design

---
+++ src/components/Footers/FooterSmall.svelte
@@ -0,0 +1,36 @@
+<script>
+  // make dynamic date to be added to footer
+  let date = new Date().getFullYear();
+
+  export let absolute = false;
+</script>
+
+<footer
+  class="pb-6 {absolute ? 'absolute w-full bottom-0 bg-green-200' : 'relative'}"
+>
+  <div class="container mx-auto px-4">
+    <!-- <hr class="mb-6 border-b-0 border-gray-200" /> -->
+    <div class="flex flex-wrap items-center md:justify-between justify-center">
+      <div class="w-full md:w-4/12 px-4">
+        <div class="text-sm text-white font-semibold py-1 text-center md:text-left">
+          GrowwMore © {date}
+        </div>
+      </div>
+      <div class="w-full md:w-8/12 px-4">
+        <ul class="flex flex-wrap list-none md:justify-end justify-center">
+          <li class="flex items-center text-sm text-white font-semibold uppercase py-1 text-center md:text-left">
+            Raj Chhaniyara & Raj Patel
+          </li>
+          <!-- <li>
+            <a
+              href="https://github.com/creativetimofficial/notus-svelte/blob/main/LICENSE.md?ref=ns-footer-small"
+              class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+            >
+              MIT License
+            </a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ src/views/Index.svelte
@@ -0,0 +1,55 @@
+<script>
+  // import { Router, Route } from "svelte-routing";
+  import { link } from "svelte-routing";
+  // components for this layout
+  import AuthNavbar from "components/Navbars/AuthNavbar.svelte";
+  import FooterSmall from "components/Footers/FooterSmall.svelte";
+
+  // pages for this layout
+  import Login from "views/auth/Login.svelte";
+
+  const registerBg2 = "../assets/img/register_bg_1.png";
+  // core components
+  const logo = "/assets/img/gm_logo.svg";
+  const github = "/assets/img/github.svg";
+</script>
+
+<!-- <script>
+  import { link } from "svelte-routing";
+
+  // core components
+  const logo = "/assets/img/gm_logo.svg";
+  const github = "/assets/img/github.svg";
+  const google = "/assets/img/google.svg";
+  export let location;
+</script> -->
+
+<div>
+  <AuthNavbar />
+  <main>
+    <section class="relative w-full h-full py-40 min-h-screen">
+      <div
+        class="absolute top-0 w-full h-full bg-green-200 bg-no-repeat bg-full"
+        style="background-image: url({registerBg2});"
+      ></div>
+      <section class="header relative pt-0 mt-25 max-h-860-px">
+        <div class="container center mx-auto px-4 h-full">
+          <div class="w-full md:w-6/12 mx-auto lg:w-6/12 xl:w-6/12 px-4">
+            <!-- <div class="pt-12 sm:pt-0"> -->
+              <img class="relative top-0 b-auto center" src="{logo}" alt=".." />
+              <h2 class="font-semibold text-center text-4xl text-black" style="font-family: Matryoshka">
+                GROWWMORE<sup>©</sup>
+              </h2>
+              <h4 class="font-semibold text-center text-2xl text-white">
+                Investing is for everyone. We're here to help you
+              </h4>
+            <!-- </div> -->
+          </div>
+        </div>
+      </section>
+      <FooterSmall absolute="true" />
+    </section>
+  </main>
+</div>
+
+
